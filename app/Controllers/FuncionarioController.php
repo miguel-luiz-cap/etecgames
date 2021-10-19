494e7d5b9b75be05688c2dbad6b95c8b11e6a997
Miguel Luiz Capristano <71992751+miguel-luiz-cap@users.noreply.github.com>
2021-10-19 20:38:07 -0300

    Correção da função

--- app/Controllers/FuncionarioController.php
+++ app/Controllers/FuncionarioController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 class FuncionarioController extends BaseController {
 	public function index()
 	{
-		inserirFuncionario();
+		return redirect()->to(base_url("FuncionarioController/inserirFuncionario"));
 	}
 
 	public function inserirFuncionario()
